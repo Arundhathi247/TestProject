713d5f6fcf2357bcbea16439bb76b2eccbc51a36
Aru <Aru@DESKTOP-NI3B95M.attlocal.net>
2022-02-03 17:49:17 -0800

    commit

---
+++ JPAEntities/src/model/TBL_TICKET.java
@@ -0,0 +1,83 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the TBL_TICKET database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="TBL_TICKET.findAll", query="SELECT t FROM TBL_TICKET t")
+public class TBL_TICKET implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int ticketid;
+	private String destinationstation;
+	private String email;
+	private String passengername;
+	private String sourcestation;
+	private Date traveldate;
+
+	public TBL_TICKET() {
+	}
+
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public int getTicketid() {
+		return this.ticketid;
+	}
+
+	public void setTicketid(int ticketid) {
+		this.ticketid = ticketid;
+	}
+
+
+	public String getDestinationstation() {
+		return this.destinationstation;
+	}
+
+	public void setDestinationstation(String destinationstation) {
+		this.destinationstation = destinationstation;
+	}
+
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassengername() {
+		return this.passengername;
+	}
+
+	public void setPassengername(String passengername) {
+		this.passengername = passengername;
+	}
+
+
+	public String getSourcestation() {
+		return this.sourcestation;
+	}
+
+	public void setSourcestation(String sourcestation) {
+		this.sourcestation = sourcestation;
+	}
+
+
+	@Temporal(TemporalType.DATE)
+	public Date getTraveldate() {
+		return this.traveldate;
+	}
+
+	public void setTraveldate(Date traveldate) {
+		this.traveldate = traveldate;
+	}
+
+}
